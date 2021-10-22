12d966d4d26bca0fe976e29829d6bbe94a7e37c9
Yaroslav Kovalev <yakovaldos@yandex.ru>
2021-10-22 19:28:47 +0300

    finished responsive layout

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,28 @@
 const swiper = new Swiper(".reviews__slider", {
   loop: false,
   speed: 800,
-  slidesPerView: 4,
-  slidesPerGroup: 4,
+  slidesPerView: 1,
+  slidesPerGroup: 1,
   spaceBetween: 30,
   watchOverflow: false,
   navigation: {
     nextEl: ".reviews__btn-next",
-    prevEl: ".reviews__btn-prev"
+    prevEl: ".reviews__btn-prev",
+  },
+  breakpoints: {
+    480: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    768: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+    }
   },
 });
 
-new Accordion('.faq__accordion');
+new Accordion(".faq__accordion");
